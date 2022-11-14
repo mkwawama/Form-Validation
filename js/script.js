e57bac6bc1a45103f356514a37e688535551e889
mkwawama <79860095+mkwawama@users.noreply.github.com>
2022-11-14 16:05:43 +0000

    Added Validation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('Script Loaded');
+var signUpForm = document.querySelector('#signUpForm');
+var usernameField= document.querySelector('#usernameField');
+var userNameErrorMessage= document.querySelector('#usernameErrorMessage');
+
+signUpForm.addEventListener('submit',(event)=>{
+    event.preventDefault();
+
+    if(signUpForm.username.value ==false){
+        usernameField.classList.add('errorField')
+        userNameErrorMessage.classList.remove('hidden')
+        console.log('You need to enter a username');
+    }
+});
